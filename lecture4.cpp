01fb3d482146384e99eb6128ec093a4857433d7a
siddharthkabade7 <siddharth.kabade.7@gmail.com>
2026-07-16 22:00:02 +0530

    trial

---
+++ lecture4.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+
+using namespace std;
+
+// int main(){
+//     int n=5;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n=4;
+//     for(int i=1;i<=n;i++){
+//         char ch='A';
+//         for(int j=0;j<=n;j++){
+//             cout<<ch;
+//             ch++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n=4;
+//     int num=1;
+//     for(int i=0;i<n;i++){
+        
+//         for(int j=0;j<n;j++){
+//             cout<<num;
+//             num++;
+//         }
+//         cout<<endl;
+// }
+// return 0;
+// }
+
+// int main(){
+//     int n=3;
+//     char ch='A';
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             cout<<ch;
+//             ch++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n=4;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n=4;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+int main(){
+    int n=4;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j>0;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+// remianed last 3 4 problens of star and hollow space etc...
