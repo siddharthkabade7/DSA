daa862079c1e8b81d1d53983654cfb409f51bc2f
siddharthkabade7 <siddharth.kabade.7@gmail.com>
2026-07-18 21:50:29 +0530

    Add classesandobject.cpp

--- classesandobject.cpp
+++ classesandobject.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
-int main(){
 
+class employee{
+    private:
+        int empId;
+        int salary;
+    public:
+        int tow;
+        void data(int empID,int salary);
+        void gdata(){
+            cout<<"The empId is "<<empId<<endl;
+            cout<<"The salary is "<<salary<<endl;
+            cout<<"The tow is "<<tow<<endl;
+        }
+};
+
+void employee::data(int empID,int salary){
+    empId=empID;
+    this->salary=salary;
+}
+
+int main(){
+    employee sid;
+    sid.tow=8;
+    sid.data(1,10000);
+    sid.gdata();
+    return 0;
 }
